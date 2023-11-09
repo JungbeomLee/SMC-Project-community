7bec014549dec21c2f4f5fc718cc58deb845e2bc
JungbeomLee <jungbum05@naver.com>
2023-11-09 18:02:18 +0900

    FEAT(time) : Add posting time

--- views/list_project/flask_project_list.py
+++ views/list_project/flask_project_list.py
@@ -10,7 +10,7 @@ bp = Blueprint('flask_project_list', __name__, url_prefix='/listPage')
 @bp.route('/')
 def main_page() :
     with register_db.cursor(pymysql.cursors.DictCursor) as cursor :
-        cursor.execute("SELECT project_title, project_image_name, project_OneLine_info, user_email FROM project_post")
+        cursor.execute("SELECT datetime, project_title, project_image_name, project_OneLine_info, user_email FROM project_post")
         post_list = cursor.fetchall()
-        
+
     return render_template('listPage.html', post_list = post_list)

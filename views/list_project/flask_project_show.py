7f49e89ef0c6ad188194560e4bf63934854583f5
JungbeomLee <jungbum05@naver.com>
2023-11-14 02:27:38 +0900

    FEAT(post show page) : Add post show page function

--- views/list_project/flask_project_show.py
+++ views/list_project/flask_project_show.py
@@ -7,11 +7,17 @@ import os
 bp = Blueprint('flask_project_show', __name__, url_prefix='/listPage')
 
 @bp.route('/<select_board_number>')
-def main_page(select_board_number) :
-    with register_db.cursor(pymysql.cursors.DictCursor) as cursor :
-        cursor.execute("SELECT * FROM project_post WHERE project_post_id = %s", (select_board_number))
-        post_list = cursor.fetchall()
+def main_page(select_board_number):
+    try:
+        select_board_number = int(select_board_number)  # 정수 변환을 확인
+        with register_db.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("SELECT * FROM project_post WHERE project_post_id = %s", (select_board_number,))
+            post_list = cursor.fetchall()
 
-    register_db.close()
+        return render_template('postShowPage.html', post_list=post_list)
+    except Exception as e:
+        # 오류 로깅 및 처리
+        print(f"An error occurred: {e}")
+        # 적절한 오류 응답 반환
 
-    return render_template('postShowPage.html', post_list = post_list)
+    
